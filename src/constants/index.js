f70a3833b204554ae497b25d5557b049d0745673
Ayush Rakesh <devloperayush23@gmail.com>
2025-11-04 12:21:51 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,7 @@
+const navLinks = [
+            { label: 'Store' },
+            { label: 'Mac' },
+            { label: 'iPhone' },
+            { label: 'Watch' },
+            { label: 'Airpods' },
+          ];
